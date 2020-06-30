e18f1b4fa56e366a49ee8a29082d62fa0da9dae0
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-06-30 15:05:26 -0500

    Space to create components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: hsl(0, 0, 98);
+`
 
 function App() {
   return (
-    <h1>Home</h1>
+    <Container>
+      <h1>Desde Lab</h1>
+    </Container>
   );
 }
 

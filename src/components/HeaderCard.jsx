031b207473858a360277f7edc51a02ea2c131541
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-06-30 17:56:24 -0500

    [HeaderCard] Layout maked

--- src/components/HeaderCard.jsx
+++ src/components/HeaderCard.jsx
@@ -28,10 +28,76 @@ const HeaderCard = () =>{
     left: -10%;
     z-index: 2;
   `
+  const SWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 16px;
+    display: grid;
+    grid-template: repeat(6, 1fr) / repeat(5, 1fr);
+    grid-template-areas:
+      ". . . . ."
+      ". . . . ."
+      ". Header Header Header ."
+      ". Header Header Header ."
+      ". Header Header Header ."
+      "Social Social . Author Author";
+    position: absolute;
+    z-index: 4;
+    background-color: rgba(0,0,0,0.3);
+  `
+  const SHeader =  styled.div`
+    grid-area: Header;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > h1 {
+      color: white;
+      max-width: 400px;
+      font-size: 40px;
+      font-weight: 600;
+      line-height: 60px;
+      text-align: center;
+      text-shadow: 0 0 5px rgba(0,0,0,0.2);
+    }
+  `
+  const SSocial = styled.div`
+    grid-area: Social;
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-end;
+
+    & > * {
+      color: white;
+    }
+  `
+  const SAuthor = styled.div`
+    grid-area: Author;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+
+    & > * {
+      color: white;
+    }
+  `
 
   return(
     <SContainer>
       <SImageBg src='https://images.pexels.com/photos/1181275/pexels-photo-1181275.jpeg'/>
+      <SWrapper>
+        <SHeader>
+          <h1>
+            Convirtiendo ideas en realidad
+          </h1>
+        </SHeader>
+        <SSocial>
+          <span>Links sociales</span>
+        </SSocial>
+        <SAuthor>
+          <span>Pon los creditos del autor wey</span>
+        </SAuthor>
+      </SWrapper>
     </SContainer>
   )
 }

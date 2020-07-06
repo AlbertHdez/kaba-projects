d3239397510f8398e5c7ab3f3a5d69fe2ad84a0c
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-07-06 15:33:59 -0500

    [components] New root branch with themes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
+
+import theme from './themes/ClassicDark'
+const {palette} = theme;
 
 const Container = styled.div`
   width: 100%;
   min-height: 100vh;
-  background-color: hsl(0, 0, 98);
+  background-color: ${palette.containerDark};
   display: flex;
   justify-content: center;
   align-items: center;
+  color: white;
 `
 
 function App() {
